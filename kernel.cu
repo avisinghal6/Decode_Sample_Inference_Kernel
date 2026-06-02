8a0dcda0f353ab0efb42861c967532ff0149fd9e
avisinghal6 <avisinghal6@gmail.com>
2026-06-02 08:25:04 -0700

    working kernel with reduced B to reduce shared memory pressure

--- kernel.cu
+++ kernel.cu
@@ -8,11 +8,14 @@
 #include <algorithm> // std::sort, std::nth_element, std::partial_sort
 #include <cmath>
 #include <random>
+#include <curand_kernel.h>
+#include <cfloat>
 
-#define B 128
+#define B 64
 #define VOCAB 128000
 #define Temperature 0.5
 #define TopK 50
+#define NUM_BLOCKS 2000
 
 
 void topk_sampling_cpu_golden(float* input, int* output) {
@@ -64,30 +67,167 @@ void topk_sampling_cpu_golden(float* input, int* output) {
 
 }
 
+__device__  float values_topK[NUM_BLOCKS][B][TopK];
+__device__  int indices_topK[NUM_BLOCKS][B][TopK];
+__device__ int block_counter = 0;
+
+__global__ void topK_sampling_gpu(float* input, int* output) {
+
+	int tid = threadIdx.x;
+	int bid = blockIdx.x;
+
+	int offset = bid * (VOCAB/NUM_BLOCKS);
+
+
+	for (int i = 0;i < (VOCAB / NUM_BLOCKS);i++) {
+		input[offset + i + tid*VOCAB] /= Temperature;
+	}
+
+
+	float last_maximum = 100000000;
+	int idx = 0;
+	for (int i = 0;i < TopK && idx<TopK;i++) {
+		float maximum= -100000000;
+		
+		for (int j = 0;j < (VOCAB / NUM_BLOCKS);j++) {
+			if (input[offset + tid * VOCAB + j] >= maximum && input[offset + tid * VOCAB + j] < last_maximum) {
+			
+				maximum = input[offset + tid * VOCAB + j];
+			}
+		}
+
+		for (int k = 0;k < (VOCAB / NUM_BLOCKS) && idx<TopK;k++) {
+			if (input[offset + tid * VOCAB + k] == maximum) {
+				values_topK[bid][tid][idx] = maximum;
+				indices_topK[bid][tid][idx] = offset+k;
+				idx += 1;
+			}
+		}
+			
+
+		last_maximum = maximum;
+	}
+
+	__threadfence();
+	int arrived = -1;
+	__shared__ int last;
+
+	if (threadIdx.x == 0) {
+		arrived = atomicAdd(&block_counter, 1);
+		last = (arrived == NUM_BLOCKS - 1);
+	}
+
+	__syncthreads();
+
+	__shared__  float final_values_topK[B][TopK];
+	__shared__  int final_indices_topK[B][TopK];
+	last_maximum = 100000000;
+	if (last) {
+
+		idx = 0;
+		for (int i = 0;i < TopK && idx < TopK;i++) {
+			float maximum = -100000000;
+			for (int j = 0;j < NUM_BLOCKS;j++) {
+
+				for (int k = 0;k < TopK;k++) {
+					if (values_topK[j][tid][k] >= maximum && values_topK[j][tid][k] < last_maximum) {
+
+						maximum = values_topK[j][tid][k];
+					}
+				}
+			}
+
+			for (int j = 0;j < NUM_BLOCKS;j++) {
+				for (int k = 0;k < TopK && idx < TopK;k++) {
+					if (values_topK[j][tid][k] == maximum) {
+						final_values_topK[tid][idx] = maximum;
+						final_indices_topK[tid][idx] = indices_topK[j][tid][k];
+						idx += 1;
+					}
+				}
+			}
+
+			last_maximum = maximum;
+
+		}
+
+
+		float maximum = final_values_topK[tid][0];
+		float denominator = 0.0;
+		for (int i = 0;i < TopK;i++) {
+			final_values_topK[tid][i] = expf(final_values_topK[tid][i] - maximum);
+			denominator += final_values_topK[tid][i];
+		}
+
+		float cdf = 0.0;
+		curandState state;
+		curand_init(0, tid, 0, &state);
+		float random_sample = curand_uniform(&state);
+
+		for (int i = 0;i < TopK;i++) {
+
+			cdf += final_values_topK[tid][i] / denominator;
+
+			if (random_sample < cdf) {
+				output[tid] = final_indices_topK[tid][i];
+				break;
+			}
+		}
+
+
+	}
+	
+}
 
 int main() {
 
 
 
-	float* input = new float[B * VOCAB];
-	int* output = new int[B];
+	float* h_input = new float[B * VOCAB];
+	int* h_output_cpu = new int[B];
+	int* h_output = new int[B];
+
+	float* d_input;
+	int* d_output;
+
+	cudaMalloc((void**)&d_input, B * VOCAB * sizeof(float));
+	cudaMalloc((void**)&d_output, B * sizeof(int));
+	
+
 	std::mt19937 gen(123);
 	std::normal_distribution<float> d(0.0f, 1.0f);
 
 	for (int i = 0; i < B * VOCAB;i++) {
-		input[i] = d(gen);
+		h_input[i] = d(gen);
 	}
 
 
-	topk_sampling_cpu_golden(input, output);
+	topk_sampling_cpu_golden(h_input, h_output);
+
+	cudaMemcpy(d_input, h_input, B * VOCAB * sizeof(float), cudaMemcpyHostToDevice);
+
+	int block_size = B;
+	int zero = 0;
+	cudaMemcpyToSymbol(block_counter, &zero, sizeof(int));
+	topK_sampling_gpu << <NUM_BLOCKS, block_size >> > (d_input, d_output);
+
+	cudaMemcpy(h_output, d_output,B * sizeof(int), cudaMemcpyDeviceToHost);
+
+
 
 	for (int i = 0;i < B;i++) {
 
-		printf("The Batch %d index is %d\n", i, output[i]);
+		printf("The Batch %d index is %d, %d\n", i, h_output_cpu[i], h_output[i]);
 	}
 
-	delete[] input;
-	delete[] output;
+
+
+	delete[] h_input;
+	delete[] h_output_cpu;
+	delete[] h_output;
+
+	cudaFree(d_input);
+	cudaFree(d_output);
 
 }
 
